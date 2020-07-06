0b762aa97a767b085126fd7b6cba50c98f9eafbf
bunny12rabbit <bunny12rabbit@gmail.com>
2020-07-06 17:20:03 +0300

    Implemented CoroutineTicker Singleton in order to run Coroutines from non MonoBehaviour classes and preventing them to stop if gameObject that runs coroutine gets disabled

---
+++ Assets/Scripts/Tools/CoroutineTicker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class CoroutineTicker : MonoBehaviour
+{
+    public static CoroutineTicker Instance { get; private set; }
+
+    private void Awake()
+    {
+        InitializeInstance();
+    }
+
+    private void InitializeInstance()
+    {
+        if (Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+}
