c83d7f8d80bcf6fc52bf335eb45b5e3512c012b8
bunny12rabbit <bunny12rabbit@gmail.com>
2020-07-06 16:42:43 +0300

    Added NativeTechTools menu with useful stuff, such as setting password for keystore at Unity launch, switching FULL_LOG which determinate whether DebugWrapper is gonna be verbose. Added SceneViewWindow, which displays all scenes, included in build settings. Can press on desired one and load it in Editor, instead of searching one.

---
+++ Assets/Editor/NativeTechTools.cs
@@ -0,0 +1,129 @@
+﻿using UnityEditor;
+
+public class NativeTechTools : EditorWindow
+{
+    public const string NATIVE_TECH_TOOLS = "Native Tech Tools";
+    public const string NATIVE_TECH_TOOLS_SUB_MENU = "Native Tech Tools/";
+
+    private const string SET_PASSWORD = "Set Password";
+    private const string SWITCH_FULL_LOG = "Switch FULL_LOG";
+
+    private const string FULL_LOG = "FULL_LOG";
+    private static bool _isFullLog;
+
+    private const string PASSWORD = "pass4root";
+
+
+    [InitializeOnLoad]
+    private class StartApp
+    {
+        static StartApp()
+        {
+            _isFullLog = ContainsDefine(FULL_LOG);
+
+            SetPassword();
+        }
+    }
+
+    #region SetPassword
+
+    [MenuItem(NATIVE_TECH_TOOLS_SUB_MENU + SET_PASSWORD)]
+    private static void SetPassword()
+    {
+        PlayerSettings.keyaliasPass = PASSWORD;
+        PlayerSettings.keystorePass = PASSWORD;
+    }
+
+    #endregion
+
+    #region Switch FULL_LOG
+
+    [MenuItem(NATIVE_TECH_TOOLS_SUB_MENU + SWITCH_FULL_LOG)]
+    private static void SwitchFullLog()
+    {
+        _isFullLog = !_isFullLog;
+
+        if (_isFullLog)
+        {
+            AddNewDefine(FULL_LOG);
+        }
+        else
+        {
+            RemoveDefine(FULL_LOG);
+        }
+    }
+
+    [MenuItem(NATIVE_TECH_TOOLS_SUB_MENU + SWITCH_FULL_LOG, true)]
+    private static bool SwitchFullLogValidation()
+    {
+        Menu.SetChecked(NATIVE_TECH_TOOLS_SUB_MENU + SWITCH_FULL_LOG, _isFullLog);
+        return true;
+    }
+
+    #endregion
+
+    #region Defines helpers
+
+    private static bool ContainsDefine(string define)
+    {
+        string defineString = GetDefineString();
+        return defineString.Contains(define);
+    }
+
+    private static string GetDefineString()
+    {
+#if UNITY_ANDROID
+            return PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android);
+#elif UNITY_IOS
+            return PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS);
+#elif UNITY_STANDALONE
+        return PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone);
+#endif
+        return string.Empty;
+    }
+
+    private static void AddNewDefine(string newDefine)
+    {
+        string defineString = GetDefineString();
+
+        if (ContainsDefine(newDefine))
+        {
+            return;
+        }
+
+        string newDefineString = $"{defineString};{newDefine};";
+#if UNITY_ANDROID
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, newDefineString);
+#elif UNITY_IOS
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS, newDefineString);
+#elif UNITY_STANDALONE
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, newDefineString);
+#endif
+    }
+
+    private static void RemoveDefine(string defineToRemoved)
+    {
+        string defineString = GetDefineString();
+        var defineArray = defineString.Split(';');
+        var newDefineString = string.Empty;
+
+        foreach (string define in defineArray)
+        {
+            if (!define.Equals(defineToRemoved))
+            {
+                newDefineString = newDefineString.Equals(string.Empty)
+                    ? $"{define};"
+                    : $"{newDefineString}{define};";
+            }
+        }
+#if UNITY_ANDROID
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, newDefineString);
+#elif UNITY_IOS
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS, newDefineString);
+#elif UNITY_STANDALONE
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, newDefineString);
+#endif
+    }
+
+    #endregion
+}
---
+++ Assets/Editor/SceneViewWindow.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+/// <summary>
+/// SceneViewWindow class.
+/// </summary>
+public class SceneViewWindow : EditorWindow
+{
+    private const string SCENE_VIEW = "Scene View";
+
+    /// <summary>
+    /// Tracks scroll position.
+    /// </summary>
+    private Vector2 _scrollPos;
+
+    /// <summary>
+    /// Initialize window state.
+    /// </summary>
+    [MenuItem(NativeTechTools.NATIVE_TECH_TOOLS_SUB_MENU + SCENE_VIEW)]
+    internal static void Init()
+    {
+        // EditorWindow.GetWindow() will return the open instance of the specified window or create a new
+        // instance if it can't find one. The second parameter is a flag for creating the window as a
+        // Utility window; Utility windows cannot be docked like the Scene and Game view windows.
+        var window = (SceneViewWindow)GetWindow(typeof(SceneViewWindow), false, "Scene View");
+        window.position = new Rect(window.position.xMin + 100f, window.position.yMin + 100f, 200f, 400f);
+    }
+
+    /// <summary>
+    /// Called on GUI events.
+    /// </summary>
+    internal void OnGUI()
+    {
+        EditorGUILayout.BeginVertical();
+        _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, false, false);
+
+        GUILayout.Label("Scenes In Build", EditorStyles.boldLabel);
+        for (var i = 0; i < EditorBuildSettings.scenes.Length; i++)
+        {
+            var scene = EditorBuildSettings.scenes[i];
+            if (!scene.enabled)
+            {
+                continue;
+            }
+
+            string sceneName = Path.GetFileNameWithoutExtension(scene.path);
+            bool isPressed = GUILayout.Button($"{i}: {sceneName}",
+                new GUIStyle(GUI.skin.GetStyle("Button")) {alignment = TextAnchor.MiddleLeft});
+
+            if (isPressed)
+            {
+                if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                {
+                    EditorSceneManager.OpenScene(scene.path);
+                }
+            }
+        }
+
+        EditorGUILayout.EndScrollView();
+        EditorGUILayout.EndVertical();
+    }
+}
