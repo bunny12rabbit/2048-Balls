c36e342dc42b08a2eaee585db53266d727347c69
bunny12rabbit <bunny12rabbit@gmail.com>
2020-07-23 15:16:12 +0300

    Implemented MatchableItemFactoryGeneric

---
+++ Assets/Scripts/Generics/Factories/MatchableItemFactoryGeneric.cs
@@ -0,0 +1,22 @@
+﻿using Generics;
+using ObjectPools;
+using UnityEngine;
+
+namespace Factories
+{
+    public class MatchableItemFactoryGeneric<T> where T : struct
+    {
+        public IMatchableItem<T> ConstructItem(string path) => ConstructItem(path, Vector2.zero);
+        
+        public IMatchableItem<T> ConstructItem(string path, Vector2 position)
+        {
+            var objectFromPool = GameObjectPool.GetObjectFromPool(path);
+            objectFromPool.transform.position = position;
+
+            var iMatchableItem = objectFromPool.GetComponent<IMatchableItem<T>>();
+            iMatchableItem.CollisionHandler.SwitchPhysicsSimulation(false);
+
+            return iMatchableItem;
+        }
+    }
+}
