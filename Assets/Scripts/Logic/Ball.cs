8ab3f898421a9828cb9ecab51c360812c4eee750
bunny12rabbit <bunny12rabbit@gmail.com>
2020-07-23 15:13:45 +0300

    Moved Ball.cs to Logic namespace + refactoring. Ball prefab, BallDataCollection and Bouncy physicsMaterial

---
+++ Assets/Scripts/Logic/Ball.cs
@@ -0,0 +1,157 @@
+﻿using Data;
+using Generics;
+using InputSystems;
+using ObjectPools;
+using Pixelplacement;
+using TMPro;
+using UnityEngine;
+
+namespace Logic
+{
+    public class Ball : MatchableItemItemGeneric<uint>
+    {
+        [SerializeField] private TMP_Text label;
+        [SerializeField] private CollisionHandlerNumeric collisionHandler;
+        [SerializeField] private LineRenderer lineRenderer;
+        [SerializeField] private SpriteRenderer spriteRenderer;
+
+        [SerializeField] private float moveTowardsDuration = 0.5f;
+        [SerializeField] private string vfxPath = "Prefabs/CFX2_RockHit";
+
+        private Ball _matchedBall;
+        private bool _isInTransition;
+        private string _initialName;
+
+
+        public BallMatchableData BallData => (BallMatchableData)Data;
+        private Transform SpriteRendererTransform { get; set; }
+
+
+        public override CollisionHandlerGeneric<uint> CollisionHandler
+        {
+            get => collisionHandler;
+            protected set => collisionHandler = (CollisionHandlerNumeric)value;
+        }
+
+        public override void OnMatch(IMatchableItem<uint> matchedObject)
+        {
+            MatchBalls(matchedObject);
+        }
+
+
+        public override void InitializeComponents()
+        {
+            if (CollisionHandler)
+            {
+                CollisionHandler.Initialize(this);
+            }
+            else
+            {
+                DebugWrapper.LogError($"{nameof(CollisionHandler)} is not found! Assign the reference!",
+                    DebugColors.Red);
+            }
+
+            SpriteRendererTransform = spriteRenderer.GetComponent<Transform>();
+        }
+
+        private void OnEnable()
+        {
+            InputListener.OnRelease += OnRelease;
+            UpdateData();
+        }
+
+        private void OnDisable()
+        {
+            InputListener.OnRelease -= OnRelease;
+            SetDefaultState();
+        }
+
+        private void SetDefaultState()
+        {
+            _isInTransition = false;
+            _matchedBall = null;
+            Data = null;
+
+            lineRenderer.enabled = true;
+        }
+
+        private void OnRelease()
+        {
+            lineRenderer.enabled = false;
+        }
+
+        private void MatchBalls(IMatchableItem<uint> matchedBall)
+        {
+            if (matchedBall.Data.IsMatched)
+            {
+                return;
+            }
+
+            Data.IsMatched = true;
+
+            SwitchPhysics(false);
+            _matchedBall = (Ball)matchedBall;
+            _matchedBall.SwitchPhysics(false);
+
+            DebugWrapper.LogError(
+                $"{name}.IsMatched={Data.IsMatched} collides with {_matchedBall.gameObject.name}.IsMatched={_matchedBall.Data.IsMatched}",
+                DebugColors.Red);
+            MoveTowardsOther(_matchedBall);
+        }
+
+        private void MoveTowardsOther(IMatchableItem<uint> matchedBall)
+        {
+            Tween.Position(transform, matchedBall.transform.position, moveTowardsDuration, 0, Tween.EaseInOutStrong,
+                Tween.LoopType.None, null, OnMoveTweenAnimationComplete);
+        }
+
+        private void OnMoveTweenAnimationComplete()
+        {
+            GameObjectPool.GetObjectFromPool(vfxPath, transform.position, Quaternion.identity);
+            _matchedBall.gameObject.PushBackToPool();
+            StartBallTransition();
+        }
+
+        private void SwitchPhysics(bool state) => CollisionHandler.SwitchPhysics(state);
+
+        private void StartBallTransition()
+        {
+            _isInTransition = true;
+            UpdateData();
+            Tween.LocalScale(SpriteRendererTransform, BallData.LocalScale, 0.2f, 0,
+                Tween.EaseBounce);
+            Tween.Color(spriteRenderer, BallData.Color, 0.2f, 0, Tween.EaseInOutStrong, Tween.LoopType.None, null,
+                OnTransitionTweenComplete);
+        }
+
+        private void OnTransitionTweenComplete()
+        {
+            _isInTransition = false;
+            Data.IsMatched = false;
+            _matchedBall = null;
+            SwitchPhysics(true);
+        }
+
+        private void UpdateData()
+        {
+            if (Data == null)
+            {
+                Data = new BallMatchableData(2);
+                _initialName = name;
+            }
+            else
+            {
+                Data.UpdateData();
+            }
+
+            label.text = Data.Criteria.ToString();
+
+            if (!_isInTransition)
+            {
+                spriteRenderer.color = BallData.Color;
+            }
+
+            name = _initialName + Data.Criteria;
+        }
+    }
+}
