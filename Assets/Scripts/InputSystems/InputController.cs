de3cfb65e135dd8cba76e901c2f8f970bcd0bcd0
bunny12rabbit <bunny12rabbit@gmail.com>
2020-07-23 15:10:05 +0300

    Implemented InputController and InputListener

---
+++ Assets/Scripts/InputSystems/InputController.cs
@@ -0,0 +1,105 @@
+﻿using Generics;
+using Interfaces;
+using Logic;
+using UnityEngine;
+
+namespace InputSystems
+{
+    public class InputController : SingletonBehaviourGeneric<InputController>, IInputHandler
+    {
+        [SerializeField] private Camera mainCamera;
+
+        private Ball _target;
+        private CollisionHandlerGeneric<uint> _collisionHandler;
+
+        private Vector3 _offset;
+        private Vector3 _newPosition;
+        private Vector3 _initialPosition;
+        private Vector3 _initialTargetPosition;
+
+        private bool _isControlled;
+        private bool _isCanControl;
+        
+
+        private void OnEnable()
+        {
+            SubscribeInputEvents();
+        }
+
+        private void OnDisable()
+        {
+            UnSubscribeInputEvents();
+        }
+
+        private void SubscribeInputEvents()
+        {
+            InputListener.OnPress += OnPress;
+            InputListener.OnRelease += OnRelease;
+            InputListener.OnDrag += OnDrag;
+        }
+
+        private void UnSubscribeInputEvents()
+        {
+            InputListener.OnPress -= OnPress;
+            InputListener.OnRelease -= OnRelease;
+            InputListener.OnDrag -= OnDrag;
+        }
+
+        public void OnPress(Vector3 position)
+        {
+            if (!_isCanControl)
+            {
+                return;
+            }
+
+            _isControlled = true;
+            _initialPosition = GetNewWorldPosition(position);
+            _initialTargetPosition = _target.transform.position;
+            _offset = _initialPosition;
+        }
+
+        public void OnDrag(Vector3 position)
+        {
+            if (!_isControlled)
+            {
+                return;
+            }
+
+            _newPosition = GetNewWorldPosition(position) - _offset;
+            DebugWrapper.Log($"WorldPosition: {_newPosition}", DebugColors.Green);
+        }
+
+        public void OnRelease()
+        {
+            if (!_isCanControl && !_isControlled)
+            {
+                return;
+            }
+
+            _collisionHandler.SwitchPhysics(true);
+            _isControlled = false;
+            _isCanControl = false;
+        }
+
+
+        public void UpdateTarget(Ball newTarget)
+        {
+            _target = newTarget;
+            _isCanControl = true;
+
+            _collisionHandler = _target.CollisionHandler;
+        }
+
+        private void FixedUpdate()
+        {
+            if (!_isControlled)
+            {
+                return;
+            }
+
+            _collisionHandler.MoveToPosition(_initialTargetPosition, _newPosition);
+        }
+
+        private Vector3 GetNewWorldPosition(Vector3 position) => mainCamera.ScreenToWorldPoint(position);
+    }
+}
---
+++ Assets/Scripts/InputSystems/InputListener.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+namespace InputSystems
+{
+    public class InputListener : MonoBehaviour
+    {
+        public static event Action<Vector3> OnPress;
+        public static event Action<Vector3> OnDrag;
+        public static event Action OnRelease;
+
+        [SerializeField] private float threshold = 2f;
+
+
+        private Vector2 _lastInputPosition;
+
+        private float SqrThreshold => threshold * threshold;
+        private static bool IsPressed => Input.GetMouseButtonDown(0);
+        private static bool IsReleased => Input.GetMouseButtonUp(0);
+        private static bool IsHeld => Input.GetMouseButton(0);
+        private bool IsDrag => IsHeld && Mathf.Abs(Input.mousePosition.sqrMagnitude) >= SqrThreshold;
+
+        private void Update()
+        {
+            var inputPosition = Input.mousePosition;
+
+            if (IsPressed)
+            {
+                UpdateLastPosition();
+            
+                OnPress?.Invoke(inputPosition);
+            }
+
+            if (IsDrag)
+            {
+                DebugWrapper.Log($"Delta: {inputPosition}", DebugColors.Gray);
+                UpdateLastPosition();
+            
+                OnDrag?.Invoke(inputPosition);
+            }
+
+            if (IsReleased)
+            {
+                OnRelease?.Invoke();
+            }
+        }
+
+        private void UpdateLastPosition()
+        {
+            _lastInputPosition = Input.mousePosition;
+        }
+    }
+}
