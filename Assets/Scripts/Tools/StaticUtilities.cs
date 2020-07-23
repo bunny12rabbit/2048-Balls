6b6215750b9c9d0919d5030710745a6cf0b5dca5
bunny12rabbit <bunny12rabbit@gmail.com>
2020-07-23 14:56:05 +0300

    Implemented StaticUtilities.cs with IsPowerOfTwo and GetPowerOfTwo from number methods

---
+++ Assets/Scripts/Tools/StaticUtilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticTools
+{
+    public static class StaticUtilities
+    {
+        public static bool IsPowerOfTwo(uint number) => number != 0 && (number & (number - 1)) == 0;
+
+        public static int GetPowerOfTwo(uint number)
+        {
+            if (number <= 0)
+            {
+                throw new ArgumentException("Number must be positive!");
+            }
+
+            return (int)Math.Log(number & -number, 2);
+        }
+    }
+}
